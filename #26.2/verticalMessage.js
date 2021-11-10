889fa3374a9b6393d9a5e1a95773d532b3b0b435
DedPerd <dedperd1@gmail.com>
2021-11-11 00:55:42 +0300

    init: start doing 'vertical message' task

---
+++ #26.2/verticalMessage.js
@@ -0,0 +1,20 @@
+'use strict'
+function showVerticalMessage(message) {
+    if(typeof(message) !== 'string') {
+        console.log(`TypeError: must be string, not ${typeof(message)}`);
+        return `TypeError: must be string, not ${typeof(message)}`;
+    }
+
+    let verticalMessage = '';
+    message[0].toUpperCase() + message.slice(1);
+    for(let i = 0; i < 10; i++) {
+        if(message[i]) {
+            verticalMessage += `${message[i]}\n`;
+        } else {
+            break;
+        }
+    }
+    console.log(verticalMessage);
+    return verticalMessage;
+}
+showVerticalMessage('марафон')
