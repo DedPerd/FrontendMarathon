9e0c7450925a2838186df771a6ddc16d3ca7f4a8
DedPerd <dedperd1@gmail.com>
2021-11-16 22:37:07 +0300

    refactor(#23 TODO list): change structure of the project feat(#23 TODO list): add the ability to sort the output of the showList function by priority and status

--- #23/TODOlist.js
+++ #23/TODOlist.js
@@ -1,57 +1,113 @@
 'use strict'
-const list = {
-    'create a task': 'In Progress',
-    'make a bed': 'Done',
-    'write a post': 'To Do',
-};
-const statuseslist = {
-    'To Do': 1,
-    'In Progress': 2,
-    'Done': 3,
+const list = [ 
+    { 
+        name: 'create a post', 
+        status: 'To Do',
+        priority: 'low'  
+    }, 
+    { 
+        name: 'create a task', 
+        status: 'Done', 
+        priority: 'high'  
+    },
+    { 
+        name: 'make a bed', 
+        status: 'In Progress', 
+        priority: 'high'  
+    }
+];
+const statuseslist = ['To Do', 'In Progress', 'Done'];
+const prioritylist = ['high', 'low']
+
+function findTaskIndex(taskName) {
+    return list.findIndex(item => item.name === taskName);
 }
-function changeStatus(task, status) {
-    if(String(task) in list) {
-        if(status in statuseslist) {
-            list[String(task)] = status;
+function Task(taskName, status, priority) {
+        this.name = taskName; 
+        this.status = status; 
+        this.priority = priority;
+}
+function createTask(taskName, status = 'To Do', priority = 'low') {
+    const index = findTaskIndex(taskName);
+    if(index === -1) {
+        if(statuseslist.includes(status) && prioritylist.includes(priority)) {
+            list.push(new Task(taskName, status, priority));
+        } else {
+            console.log('Error: invalid argument');
         }
     } else {
-        console.log('Error: Such task does not exist');
-        return 'Error: Such task does not exist';
+        console.log('Error: such task already exist');
     }
 }
-function addTask(task) {
-    if(!(task in list)) {
-        list[task] = 'To Do';
+function deleteTask(taskName) {
+    const index = findTaskIndex(taskName);
+    if(index !== -1) {
+        list.splice(index, 1);
     } else {
-        console.log('Error: Such task already exists');
-        return 'Error: Such task already exists';
+        console.log('Error: such task does not exist');
     }
-    
 }
-function deleteTask(task) {
-    if(task in list) {
-        delete list[task];
+function changeStatus(taskName, status) {
+    const taskIndex = findTaskIndex(taskName);
+    const statusIndex = statuseslist.indexOf(status);
+    if(taskIndex !== -1) {
+        if(statuseslist.includes(status)) {
+            list[taskIndex].status = statuseslist[statusIndex];
+        } else {
+            console.log('Error: invalid argument');
+        }
     } else {
-        console.log('Error: Such task does not exist');
-        return 'Error: Such task does not exist';
+        console.log('Error: such task does not exist');
     }
 }
-function showList() {
+function changePriority(taskName, priority) {
+    const taskIndex = findTaskIndex(taskName); 
+    if(prioritylist.includes(priority)) {
+        list[taskIndex].priority = priority;
+    } else {
+        console.log('Error: invalid argument');
+    }
+}
+function showListBy(sortBy = 'status') {
     let result = '';
     let isThereSth = false;
-    for(let status in statuseslist){
-        result += `${status}:\n`;
-        isThereSth = false;
-        for(let task in list) {
-            if(list[task] === status) {
-                isThereSth = true;
-                result += `"${task}",\n`;
+    switch(sortBy) {
+        case 'status':
+            for(let key of statuseslist) {
+                result += `${key}:\n`;
+                isThereSth = false;
+                list.forEach(item => {
+                    if(item.status === key) {
+                        result += `  ${item.name},\n`;
+                        isThereSth = true;
+                    }
+                });
+                if(!isThereSth) {
+                    result += '  -\n';
+                };
+            }        
+            break;
+        case 'priority':
+            for(let key of prioritylist) {
+                isThereSth = false;
+                result += `${key}:\n`;
+                list.forEach(item => {
+                    if(item.priority === key) {
+                        result += `  ${item.name},\n`;
+                        isThereSth = true;
+                    }
+                });
+                if(!isThereSth) {
+                    result += `  -\n`;
+                }
             }
-        }
-        if(!isThereSth) {
-            result += '-\n';
-        }
+            break;
+        default:
+            console.log('Error: invalid argument');
+            break;
     }
     console.log(result);
-    return result;
 }
+
+
+
